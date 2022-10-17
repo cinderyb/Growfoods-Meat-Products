49762384a5c4de8acb7f0a69a51615510987c16d
Cinder <cinderbayongasan@gmail.com>
2022-10-17 14:46:09 +0800

    Added pork spare ribs, finalized nav bar drop down menu

---
+++ index.js
@@ -0,0 +1,26 @@
+// const meats = [
+//    {
+//       "id": 1,
+//       "name": "Pork Jowls",
+//       "img-src": "images/pork/pork jowl.png",
+//       "price": "P510/kg"
+//    },
+//    {
+//       "id": 2,
+//       "name": "Pork Belly",
+//       "img-src": "images/pork/pork jowl.png",
+//       "price": "P510/kg"
+//    },
+//    {
+//       "id": 3,
+//       "name": "Beef Belly",
+//       "img-src": "images/pork/pork jowl.png",
+//       "price": "P510/kg"
+//    },
+//    {
+//       "id": 4,
+//       "name": "Pork Chop",
+//       "img-src": "images/pork/pork jowl.png",
+//       "price": "P510/kg"
+//    }
+// ]
